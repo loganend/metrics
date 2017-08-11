919050ed52ce8cce47352f8935d3ea00cb2a70f7
sergey90x <cheremisin.sergey@yandex.ru>
2017-08-12 01:07:24 +0300

    Metrics app, start

---
+++ metrics.go
@@ -0,0 +1,41 @@
+package metrics
+
+import (
+	"log"
+	"os"
+	"runtime"
+
+	"app/shared/jsonconfig"
+	"app/shared/database"
+	"app/shared/server"
+	"app/route"
+	"app/session"
+)
+
+func init(){
+	log.SetFlags(log.Lshortfile)
+	runtime.GOMAXPROCS(runtime.NumCPU())
+}
+
+func main(){
+	jsonconfig.Load("config"+string(os.PathSeparator)+"config.json", config)
+
+	session.Configure(config.Session)
+
+	database.Connect(config.Database)
+
+	server.Run(route.LoadHTTP(), route.LoadHTTPS(), config.Server)
+}
+
+var config = &configuration{}
+
+// configuration contains the application settings
+type configuration struct {
+	Database  database.Info   `json:"Database"`
+	//Email     email.SMTPInfo  `json:"Email"`
+	//Recaptcha recaptcha.Info  `json:"Recaptcha"`
+	Server    server.Server   `json:"Server"`
+	Session   session.Session `json:"Session"`
+	//Template  view.Template   `json:"Template"`
+	//View      view.View       `json:"View"`
+}
---
+++ vendor/app/shared/database/database.go
@@ -0,0 +1,67 @@
+package database
+
+import (
+	"github.com/jmoiron/sqlx"
+	"fmt"
+	"log"
+)
+
+var (
+	SQL *sqlx.DB
+
+	databases Info
+)
+
+type Type string
+
+const (
+	TypeMySQL Type = "MySQL"
+)
+
+type Info struct {
+	Type Type
+	MySQL MySQLInfo
+}
+
+type MySQLInfo struct {
+	Username  string
+	Password  string
+	Name      string
+	Hostname  string
+	Port      int
+	Parameter string
+}
+
+func DSN(ci MySQLInfo) string {
+	// Example: root:@tcp(localhost:3306)/test
+	return ci.Username +
+		":" +
+		ci.Password +
+		"@tcp(" +
+		ci.Hostname +
+		":" +
+		fmt.Sprintf("%d", ci.Port) +
+		")/" +
+		ci.Name + ci.Parameter
+}
+
+func Connect(d Info) {
+	var err error
+
+ 	databases = d
+
+	switch d.Type {
+	case TypeMySQL:
+		// Connect to MySQL
+		if SQL, err = sqlx.Connect("mysql", DSN(d.MySQL)); err != nil {
+			log.Println("SQL Driver Error", err)
+		}
+
+		// Check if is alive
+		if err = SQL.Ping(); err != nil {
+			log.Println("Database Error", err)
+		}
+	default:
+		log.Println("No registered database in config")
+	}
+}
